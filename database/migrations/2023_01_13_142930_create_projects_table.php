ddadf97232cd68b48a042d605fa5c998eda804a3
unknown <khadijaezzayany8@gmail.com>
2023-01-13 17:11:59 +0100

    create data

---
+++ database/migrations/2023_01_13_142930_create_projects_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use  App\Models\Category;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('link');
+            $table->string('image');
+            $table->string('title');
+            $table->string('discp');
+            $table->timestamps();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+       
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
