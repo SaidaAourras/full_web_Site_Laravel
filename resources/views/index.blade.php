56b3e49e6d36ebae947276f9a4367b1898edcf3d
unknown <khadijaezzayany8@gmail.com>
2023-02-07 11:07:12 +0100

    add reference

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -269,4 +269,27 @@
         </div>
     </div>
     <!-- Service End -->
+     <!-- Vendor Start -->
+     <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div class="container py-5 mb-5">
+            <div class="bg-white">
+                <div class="owl-carousel vendor-carousel">
+                    <img src="addons/img/refecences-logo_AEM-01.png" alt="">
+                    <img src="addons/img/refecences-logo_LINAGREEN-01.png" alt="">
+                    <img src="addons/img/refecences-logo_INTECA_Model2.png" alt="">
+                    <img src="addons/imgrefecences-logo_CMGP-01.png" alt="">
+                    <img src="addons/img/refecences-logo_AMAL IRRIGATION-01.png" alt="">
+                    <img src="addons/img/refecences-logo_rodamaroc.png" alt="">
+                    <img src="addons/img/refecences-logo_tecnidex.png" alt="">
+                    <img src="addons/img/refecences-logo_carrefourmanu.png" alt="">
+                    <img src="addons/img/refecences-logo_hortisud.png" alt="">
+                    <img src="addons/img/refecences-logo_rodamaroc.png" alt="">
+                    <img src="addons/img/refecences-logo_tecnidex.png" alt="">
+                    <img src="addons/img/refecences-logo_carrefourmanu.png" alt="">
+                    <img src="addons/img/refecences-logo_hortisud.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Vendor End -->
 @endsection
