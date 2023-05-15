859554c276d187744ee216571443a4c6e6617dbf
khadijaezzayany <khadijaezzayany8@gmail.com>
2023-05-15 16:37:17 +0200

    dashboard after login

--- resources/views/master/navbar.blade.php
+++ resources/views/master/navbar.blade.php
@@ -2,7 +2,7 @@
           <nav class="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0">
             <div class="container">
                 <a href="#" class="navbar-brand p-0">
-                    <img style="width: 10rem;" class="m-0" src="addons/img/logo-GMC.svg" alt="">
+                    {{-- <img style="width: 10rem;" class="m-0" src="addons/img/logo-GMC.svg" alt=""> --}}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span class="fa fa-bars"></span>
